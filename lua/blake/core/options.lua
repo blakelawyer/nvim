c4b469de5270cc50fadf04a297370bb53d414b73
blakelawyer <blakethelawyer@gmail.com>
2023-01-22 17:52:24 -0600

    personal neovim config; first commit

---
+++ lua/blake/core/options.lua
@@ -0,0 +1,24 @@
+local opt = vim.opt
+
+opt.undofile = true
+
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"

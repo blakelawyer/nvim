6ac233fcc2c300e69b8eba9946789f035436c05c
Blake Lawyer <blakethelawyer@gmail.com>
2023-02-15 22:39:20 +0000

    fixed missing icon for copilot + color

--- lua/blake/plugins/nvim-cmp.lua
+++ lua/blake/plugins/nvim-cmp.lua
@@ -20,6 +20,7 @@ end
 require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.opt.completeopt = "menu,menuone,noselect"
+vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
 
 cmp.setup({
 	snippet = {
@@ -44,11 +45,13 @@ cmp.setup({
 		{ name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
 	}),
+	-- vim.api.nvim_set_h1(0, "CmpItemKindCopilot", {fg = "#6CC644" })
 	-- configure lspkind for vs-code like icons
 	formatting = {
 		format = lspkind.cmp_format({
 			maxwidth = 50,
 			ellipsis_char = "...",
+			symbol_map = { Copilot = " " },
 		}),
 	},
 })
